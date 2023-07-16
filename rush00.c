38ef50b62a00202e8cc0391a58e0612009599bc1
xroot <62248541+kaankny@users.noreply.github.com>
2023-07-16 17:01:56 +0300

    Add files via upload

---
+++ rush00.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkanyilm <42kocaeli.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/15 11:42:17 by kkanyilm          #+#    #+#             */
+/*   Updated: 2023/07/15 16:22:33 by kkanyilm         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_printline(int len, char first, char middle, char last)
+{
+	int	i;
+
+	i = 1;
+	while (i <= len)
+	{
+		if (i == 1)
+			ft_putchar(first);
+		else if (i == len)
+			ft_putchar(last);
+		else
+			ft_putchar(middle);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	line;
+
+	line = 1;
+	if (x > 0 && y > 0)
+	{
+		while (line <= y)
+		{
+			if (line == 1)
+				ft_printline(x, 'o', '-', 'o');
+			else if (line == y)
+				ft_printline(x, 'o', '-', 'o');
+			else
+				ft_printline(x, '|', ' ', '|');
+			line++;
+		}
+	}
+}
